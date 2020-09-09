dbdfbc20617510ae68651f1b8b4cefccbd06d81d
Mahfuz <kitemahfuz@gmail.com>
2020-09-09 16:54:46 +0600

    brand new project

--- SingleView/ViewController.h
+++ SingleView/ViewController.h
@@ -9,8 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+- (IBAction)btnPressed:(id)sender;
 
 
 @end
---
+++ SingleViewTests/SingleViewTests.m
@@ -0,0 +1,37 @@
+//
+//  SingleViewTests.m
+//  SingleViewTests
+//
+//  Created by Mahfuz on 9/9/20.
+//  Copyright © 2020 KITE GAMES STUDIO. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SingleViewTests : XCTestCase
+
+@end
+
+@implementation SingleViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
