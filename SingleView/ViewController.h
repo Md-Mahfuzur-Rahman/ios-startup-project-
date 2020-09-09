064b22e30b3f1d9b1cccac0cab2b83df9d15dc1e
Mahfuz <kitemahfuz@gmail.com>
2020-09-09 17:45:43 +0600

    final

--- SingleView/ViewController.h
+++ SingleView/ViewController.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-- (IBAction)btnPressed:(id)sender;
 
 
 @end
--- SingleView/ViewController.m
+++ SingleView/ViewController.m
@@ -20,31 +20,4 @@
 }
 
 
-- (IBAction)btnPressed:(id)sender {
-
-    UIAlertController* alert = [UIAlertController alertControllerWithTitle: nil
-                                                                   message: @"test"
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    
-    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault
-                                                          handler:^(UIAlertAction * action) {
-                                                          }];
-    [alert addAction:defaultAction];
-//    UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Settings" style:UIAlertActionStyleDefault
-//
-//                                                         handler:^(UIAlertAction * action) {
-//                                                             NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
-//                                                             UIApplication *application = [UIApplication sharedApplication] ;
-//
-//                                                             if([application canOpenURL: url])
-//                                                                 [application openURL: url];
-//                                                         }
-//                                   ];
-//    [alert addAction:cancelAction];
-    
-    [self presentViewController: alert animated: YES completion: nil];
-
-}
-
-
 @end
